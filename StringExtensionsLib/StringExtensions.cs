6db9718aff57a00cd5f0094b6861a433607a674d
Chris Busse <cbusse1@my.centennialcollege.ca>
2025-04-06 11:31:00 -0400

    Add project files.

---
+++ StringExtensionsLib/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensionsLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string? str)
+        { 
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char firstChar = str[0];
+            return char.IsUpper(firstChar);
+        }
+    }
+}
